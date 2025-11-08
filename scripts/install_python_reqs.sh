f4effbba2323e916306ffb3fef1145ee43489916
knowusuboaky <64245732+knowusuboaky@users.noreply.github.com>
2025-11-08 10:38:44 -0500

    Update directory path in install script

---
+++ scripts/install_python_reqs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# install_python_reqs.sh
+
+cd /home/ec2-user/asr-fastapi-app
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install required Python packages (uncomment if you have a requirements file)
+# pip install -r requirements.txt
